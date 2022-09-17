4499a83b985f43d9f0a9f4fac22d3021beec51cf
stereosteam <moabid75@gmail.com>
2022-09-17 13:47:56 -0400

    changed some names and stuff

--- src/flashcard.py
+++ src/flashcard.py
@@ -1,20 +1,30 @@
+from typing import Union
+
 class Flashcard:
     front: str
     back: str
     gist: str
-    left: str = None
-    right: str = None
+    first: Union[None, str]
+    second: Union[None, str]
     headline: str
-    _headline: list
+    _headline_embedding: list
     _embedding: list
 
-    def init(self, front, back, gist, left, right, headline_str, headline_embed, embedding):
+    def init(self, front, back, gist, first, second, headline_str, headline_embedding, embedding):
         self.front = front
         self.back = back
         self.gist = gist
         self.headline = headline_str
-        self._headline = headline_embed
+        self._headline_embedding = headline_embedding
         self._embedding = embedding
-        self.left = left
-        self.right = right
+        self.first = first
+        self.right = second
+    
+    def get_headline_embedding(self):
+        return self._headline_embedding
+    
+    def get_embedding(self):
+        return self._embedding
+
+    
 
--- src/process_text.py
+++ src/process_text.py
@@ -1,41 +1,45 @@
 from get_transcript import get_transcript
-import spacy
+from spacy.lang.en import English
 import re
 import cohere
-
-filename = "Asking Harvard Students If They Ever Sleep.mp3"
-
+import annoy
 
 def get_prof_embedding():
     """Returns the embedding of the prof's transcript."""
     api_key = "2LMDM3GEVPLvDVoSQlm4bV5W4EbKn2ZW0jgl6zEM"
     co = cohere.Client(api_key)
     speaker_to_str = get_transcript(filename)
-    # get lst of speaker A's sentences
     prof = json_to_lst(speaker_to_str['A'])
 
-    prof_embeddings = co.embed(texts=prof,
+    prof_embedding = co.embed(texts=prof,
                                model="large",
                                truncate="RIGHT").embeddings
-    return prof_embeddings
+    return prof_embedding
 
 
 def json_to_lst(text_content: str):
     # assumption: this is the prof's transcript
-    nlp = spacy.load("en_core_web_sm", disable=["ner"])
+    tokenizer = nlp.tokenizer
     text_content = re.sub(r"\(.*\)", "", text_content)
-    doc = nlp(text_content)
+    doc = tokenizer(text_content)
     sentences = [str(x) for x in doc.sents]
-    print(sentences)
     return sentences
 
 
-def get_entity_from_headline(headline: str):
+def get_title_from_headline(headline: str):
     # get the entity from the headline
     return
 
+# "angular", "euclidean", "manhattan", "hamming", or "dot"
+def get_similiar_sentences(prof_embedding, compare):
+    # Create the search index, pass the size of embedding
+    search_index = AnnoyIndex(prof_embedding.shape[1], 'angular')
+    # Add all the vectors to the search index
+    for i in range(len(embeds)):
+        search_index.add_item(i, embeds[i])
 
-def get_similiar_sentences(prof, compar):
+    search_index.build(10) # 10 trees
+    search_index.save('transcript.ann')
     # get the similarity between prof and student embeddings
     # return the most similar sentences
     return
