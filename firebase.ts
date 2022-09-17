51a9f6edf1bc05113ccf2c42f75b3e931a72f4e1
Arihant Bapna <greatbapna@gmail.com>
2022-09-17 05:17:39 -0400

    Add login and register

---
+++ firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth }  from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBHJRN_eZF2rjlsyDGaLgpJPUNBlBxHGfc",
+    authDomain: "htn-rn-app.firebaseapp.com",
+    projectId: "htn-rn-app",
+    storageBucket: "htn-rn-app.appspot.com",
+    messagingSenderId: "254567386670",
+    appId: "1:254567386670:web:fe34a92374b04a3e2ed9bf",
+    measurementId: "G-815DPDW144"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
---
+++ pages/Home/Home.tsx
@@ -0,0 +1,77 @@
+import {
+    Button,
+    Center,
+    Heading,
+    HStack, Input,
+    NativeBaseProvider,
+    Switch,
+    Text,
+    useColorMode, useToast,
+    VStack
+} from "native-base";
+import NativeBaseIcon from "../../components/NativeBaseIcon";
+import {signInWithEmailAndPassword} from "firebase/auth";
+import React, {useState} from "react";
+import {auth} from "../../firebase";
+import {useNavigation} from "@react-navigation/native";
+
+export function HomePage(){
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const toast = useToast();
+
+    const navigation = useNavigation();
+
+    // Function try Logging in to the service
+    function LoginUser(){
+        signInWithEmailAndPassword(auth, email , password).then( (userCred) => {
+            const user = userCred.user;
+        }).catch((error) => {
+           toast.show({title: error.code, description: error.message, backgroundColor: "red.600"});
+        });
+    }
+
+    return (
+        <NativeBaseProvider>
+
+            <Center
+                _dark={{ bg: "blueGray.900" }}
+                _light={{ bg: "blueGray.50" }}
+                px={4}
+                flex={1}
+            >
+                <VStack space={5} alignItems="center">
+                    <NativeBaseIcon />
+                    <Heading size="lg">Welcome to Squirrel</Heading>
+                    <VStack space={2} width={"auto"} alignItems="center">
+                        <Input type={"text"} value={email} onChangeText={setEmail} width={"100%"} height={"40px"} variant="outline" placeholder="Email" />
+                        <Input type={"password"} value={password} onChangeText={setPassword} width={"100%"} height={"40px"} variant="outline" placeholder="Password" />
+                    </VStack>
+                    <HStack space={2}>
+                        <Button onPress={LoginUser}>Login</Button>
+                        <Button onPress={() => (navigation as any).navigate("register")}>Go to Register</Button>
+                    </HStack>
+                    <ToggleDarkMode />
+                </VStack>
+            </Center>
+        </NativeBaseProvider>
+    );
+}
+
+// Color Switch Component
+export function ToggleDarkMode() {
+    const { colorMode, toggleColorMode } = useColorMode();
+    return (
+        <HStack space={2} alignItems="center">
+            <Text>Dark</Text>
+            <Switch
+                isChecked={colorMode === "light"}
+                onToggle={toggleColorMode}
+                aria-label={
+                    colorMode === "light" ? "switch to dark mode" : "switch to light mode"
+                }
+            />
+            <Text>Light</Text>
+        </HStack>
+    );
+}
