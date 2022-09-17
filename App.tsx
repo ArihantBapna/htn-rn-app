c450e3ce75b430bea58fe9ab1d8bd69a071a2ab4
Arihant Bapna <greatbapna@gmail.com>
2022-09-17 01:51:34 -0400

    Initial commit
    Generated by create-expo-app 1.1.1.

--- App.tsx
+++ App.tsx
@@ -1,23 +1,20 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NativeWindStyleSheet } from "nativewind";
-import HomePage from "./pages/Home";
-
-
-const Stack = createNativeStackNavigator();
-
-NativeWindStyleSheet.setOutput({
-    default: "native",
-});
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="home" component={HomePage} options={{headerShown: false}} />
-        </Stack.Navigator>
-      </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
