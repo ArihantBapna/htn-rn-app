da3ac4434b1120af5db9aa1349c6f6bd2a7abfc5
stereosteam <moabid75@gmail.com>
2022-09-17 11:28:55 -0400

    goose

--- src/process_text.py
+++ src/process_text.py
@@ -1,31 +1,37 @@
-import get_prof_transcript
+from get_prof_transcript import generate_prof_transcript
 import spacy
+import re
 import cohere
 
 
-transcript = generate_prof_transcript(filename)
-
-def get_prof_embedding(speaker_to_str):
-    # assumption: only want embeddings of the prof's lecture
-    pd.set_option('display.max_colwidth', None)
-
-    # get lst of speaker A's sentences
-    test = json_to_lst(speaker_to_str['A'])
-
-    # get embeddings
+def get_prof_embedding(speaker_to_str: dict):
+    '''Returns the embedding of the prof's transcript.'''
     api_key = "2LMDM3GEVPLvDVoSQlm4bV5W4EbKn2ZW0jgl6zEM"
     co = cohere.Client(api_key)
-    embeddings_test = co.embed(texts=test,
+    transcript = generate_prof_transcript(filename)
+    # get lst of speaker A's sentences
+    prof = json_to_lst(speaker_to_str['A'])
+
+    prof_embeddings = co.embed(texts=prof,
                                model="large",
-                               truncate="LEFT").embeddings
-    return embeddings_test
+                               truncate="RIGHT").embeddings
+    return prof_embeddings
 
 
 def json_to_lst(text_content: str):
     # assumption: this is the prof's transcript
     nlp = spacy.load("en_core_web_sm", disable=["ner"])
-    # re.sub the question tags fjdlkfldkfj (X) sfjdlfdk
+    text_content = re.sub(r"\(.*\)", "", text_content)
     doc = nlp(text_content)
     sentences = [str(x) for x in doc.sents]
     print(sentences)
     return sentences
+
+def get_entity_from_headline(headline: str):
+    # get the entity from the headline
+    return
+
+def get_similiar_sentences(prof, misc):
+    # get the similarity between prof and student embeddings
+    # return the most similar sentences
+    return
