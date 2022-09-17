4309566954af9416eb420f41097e8186afb01d45
stereosteam <moabid75@gmail.com>
2022-09-17 16:02:17 -0400

    big boy changes to process_text. formatting.

--- src/process_text.py
+++ src/process_text.py
@@ -3,45 +3,105 @@ import spacy
 import re
 import cohere
 import annoy
+from flashcard import Flashcard
 
-def get_prof_embedding():
-    """Returns the embedding of the prof's transcript."""
+
+def get_prof_data(filename):
+    """Returns the structured data of the prof's transcript."""
     api_key = "2LMDM3GEVPLvDVoSQlm4bV5W4EbKn2ZW0jgl6zEM"
     co = cohere.Client(api_key)
     speaker_to_str = get_transcript(filename)
-    prof = json_to_lst(speaker_to_str['A'])
-
-    prof_embedding = co.embed(texts=prof,
-                               model="large",
-                               truncate="RIGHT").embeddings
-    return prof_embedding
+    transcript = json_to_lst(speaker_to_str["A"])
+    # entities = speaker_to_str['entities?']
+    embedding = co.embed(
+        texts=prof_transcript, model="large", truncate="RIGHT"
+    ).embeddings
+    chapters = speaker_to_str["chapters"]  # (gist, headline, summary)
+    return (embedding, transcript, entities, chapters)
 
 
 def json_to_lst(text_content: str):
     # assumption: this is the prof's transcript
+    text_content = re.sub(r"\(.*\)", "", text_content)
     nlp = English()  # just the language with no pipeline
     nlp.add_pipe("sentencizer")
     doc = nlp(text_content)
-    text_content = re.sub(r"\(.*\)", "", text_content)
-    doc = tokenizer(text_content)
-    sentences = [str(x) for x in doc.sents]
+    sentences = [x for x in doc.sents]
+    assert sentences == [str(x) for x in doc.sents]  # sanity check
     return sentences
 
 
-def get_title_from_headline(headline: str):
-    # get the entity from the headline
+def get_titles_from_chapters(chapter: Tuple[str]) -> str:
+    # get the unique keywords for each flashcard from the chapter
+    # Query LLM here to give me the key idea based on the chapter gist, summary, and headline
     return
 
-# "angular", "euclidean", "manhattan", "hamming", or "dot"
-def get_similiar_sentences(prof_embedding, compare):
-    # Create the search index, pass the size of embedding
-    search_index = AnnoyIndex(prof_embedding.shape[1], 'angular')
-    # Add all the vectors to the search index
-    for i in range(len(embeds)):
-        search_index.add_item(i, embeds[i])
 
-    search_index.build(10) # 10 trees
-    search_index.save('transcript.ann')
-    # get the similarity between prof and student embeddings
-    # return the most similar sentences
-    return
+# options for ANN "angular", "euclidean", "manhattan", "hamming", or "dot"
+def get_similiar_sentences(prof_transcript, prof_embeddings, headlines, n=3):
+    phrases_to_vectors = dict(
+        zip(prof_transcript, prof_embeddings)
+    )  # map our phrases to embeddings
+
+    seen = set()  # seen phrases, prevent duplicate similar sentences
+    similiar_sentences = (
+        set()
+    )  # key: headline, value: (headline embedding, list of top 3 phrases, list of embeddings)
+
+    search_index = AnnoyIndex(
+        prof_embeddings.shape[1], "angular"
+    )  # build ANN search tree
+    for i in range(len(prof_embedding)):
+        search_index.add_item(i, prof_embeddings[i])
+    search_index.build(10)  # 10 trees
+
+    # search_index.save('transcript.ann') # we can save this tree if needed later
+
+    # get the similarity between prof and headline embedding
+    for headline in headlines:
+        headline_embedding = co.embed(
+            texts=[headline], model="large", truncate="RIGHT"
+        ).embeddings
+        similiar_sentences[headline] = (headline_embedding, [], [])
+
+    for i in range(n):
+        for headline in headlines:
+            top = search_index.get_nns_by_vector(similar_sentences[headline][0][0], 1)[0]
+            if top in seen:
+                j = 1
+                while top in seen:
+                    top = search_index.get_nns_by_vector(similar_sentences[headline][0][0], j)[j]
+                    j += 1
+            seen.add(top)
+            similar_sentences[headline][1] += top
+            similar_sentences[headline][2] += phrases_to_vectors[top]
+
+    return similar_sentences
+
+
+def get_flashcards() -> Set[Flashcard]:
+    # get the transcript and relevant data
+    prof_data = get_prof_data(filename)  # (transcript, embeddings, entities, chapters)
+    prof_transcript = prof_data[0]
+    prof_embeddings = prof_data[1]
+    entities = prof_data[2]
+    chapters = prof_data[3]
+    # get the chapters, get the unique titles from each chapter
+    titles = [get_title_from_chapter(c) for c in chapters]
+    headlines = [c[1] for c in chapters]
+    # get the similiar sentences to each headline
+    similiar_sentences = get_similar_sentences(
+        prof_transcript, prof_embeddings, headlines
+    )
+    # ^^^ key: headline, value: (headline embedding, list of top 3 phrases, list of embeddings)
+
+    # init flashcard: need front: str
+    # back: str
+    # gist: str
+    # first: Union[None, str]
+    # second: Union[None, str]
+    # headline: str
+    # _headline_embedding: list
+    # _embedding: list
+
+    return flashcards
