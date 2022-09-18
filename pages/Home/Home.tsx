f742dde656b86771da7df88a82eb15d1cb3eb4d2
Arihant Bapna <greatbapna@gmail.com>
2022-09-18 05:58:46 -0400

    Make Home Page pretty

--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -7,7 +7,7 @@ import {
   ScrollView,
   Heading,
   HStack,
-  Icon,
+  Image,
   IconButton,
 } from "native-base";
 import { auth, db } from "../../firebase";
@@ -17,10 +17,16 @@ import { Recording } from "expo-av/build/Audio/Recording";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
 import { collection, query, where, onSnapshot } from "firebase/firestore";
-import { Linking } from "react-native";
+import {ImageBackground, Linking, TouchableOpacity, View} from "react-native";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+// @ts-ignore
+import image from "./assets/bg.png";
+// @ts-ignore
+import record from "./assets/main_button.png";
+// @ts-ignore
+import logout from "./assets/logout.png";
 
 export function HomePage() {
   const toast = useToast();
@@ -118,102 +124,106 @@ export function HomePage() {
   }, [value, loading]);
 
   return (
-    <Box height={"100%"} px={8} safeArea>
-      <Heading textAlign={"center"}>Your Recordings</Heading>
-
-      <Center my={4}>
-        <HStack justifyContent={"space-between"} alignItems={"center"}>
-          <Center>
-            {recording == null ? (
-              <Button onPress={startRecording} backgroundColor={"red.500"}>
-                Start Recording
-              </Button>
-            ) : (
-              <Button onPress={stopRecording} backgroundColor={"green.500"}>
-                Stop Recording
-              </Button>
-            )}
-          </Center>
-        </HStack>
-      </Center>
-
-      <Center my={4}>
-        <ScrollView
-          w={"100%"}
-          style={{ display: "flex", flexGrow: 1, flexDirection: "column" }}
-        >
-          {recordings.map((recording, index) => {
-            return (
-              <HStack
-                key={index}
-                justifyContent={"space-between"}
-                alignItems={"center"}
-                my={3}
-              >
-                <Heading>{recording.name}</Heading>
-                <HStack space={2} alignItems={"center"} display={"flex"}>
-                  <IconButton
-                    icon={<Ionicons name="download" size={24} color="black" />}
-                    onPress={() => {
-                      Linking.openURL(recording.url)
-                        .then()
-                        .catch((err) => {
-                          toast.show({
-                            title: "Error opening file",
-                            backgroundColor: "red.500",
+      <ImageBackground resizeMode='cover'  source={image} style={{width: "100%", height: "100%"}}>
+          <View style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center"
+          }}>
+              <Box height={"100%"} px={8} safeArea>
+                  <Center>
+                      <Heading mt={8} textAlign={"center"}>Your Recordings</Heading>
+                      <HStack justifyContent={"space-between"} alignItems={"center"}>
+                          <Center>
+                              {recording == null ? (
+                                  <TouchableOpacity onPress={startRecording}>
+                                      <Image source={record} alt="Squirrel Logo" width={"141px"} height={"56px"} />
+                                  </TouchableOpacity>
+                              ) : (
+                                  <Button onPress={stopRecording} backgroundColor={"#C98860"}>
+                                      Stop Recording
+                                  </Button>
+                              )}
+                          </Center>
+                      </HStack>
+                      <ScrollView
+                          w={"100%"}
+                          maxHeight={"75%"}
+                          style={{ display: "flex", flexGrow: 1, flexDirection: "column" }}
+                      >
+                          {recordings.map((recording, index) => {
+                              return (
+                                  <HStack
+                                      key={index}
+                                      justifyContent={"space-between"}
+                                      alignItems={"center"}
+                                      my={3}
+                                  >
+                                      <Heading>{recording.name}</Heading>
+                                      <HStack space={2} alignItems={"center"} display={"flex"}>
+                                          <IconButton
+                                              icon={<Ionicons name="download" size={24} color="black" />}
+                                              onPress={() => {
+                                                  Linking.openURL(recording.url)
+                                                      .then()
+                                                      .catch((err) => {
+                                                          toast.show({
+                                                              title: "Error opening file",
+                                                              backgroundColor: "red.500",
+                                                          });
+                                                      });
+                                              }}
+                                          />
+                                          <IconButton
+                                              onPress={() => {
+                                                  (navigation as any).navigate("transcript", {
+                                                      recording: recordings[index],
+                                                  });
+                                              }}
+                                              icon={
+                                                  <Ionicons
+                                                      name={"clipboard-outline"}
+                                                      size={24}
+                                                      color={"black"}
+                                                  />
+                                              }
+                                          />
+                                          <IconButton
+                                              onPress={() => {
+                                                  (navigation as any).navigate("slides", {
+                                                      recording: recordings[index],
+                                                  });
+                                              }}
+                                              icon={
+                                                  <Ionicons
+                                                      name={"albums-outline"}
+                                                      size={24}
+                                                      color={"black"}
+                                                  />
+                                              }
+                                          />
+                                      </HStack>
+                                  </HStack>
+                              );
+                          })}
+                      </ScrollView>
+                  </Center>
+                  <TouchableOpacity onPress={() => {
+                      signOut(auth)
+                          .then()
+                          .catch((error) => {
+                              toast.show({
+                                  title: error.code,
+                                  description: error.message,
+                                  backgroundColor: "red.600",
+                              });
                           });
-                        });
-                    }}
-                  />
-                  <IconButton
-                    onPress={() => {
-                      (navigation as any).navigate("transcript", {
-                        recording: recordings[index],
-                      });
-                    }}
-                    icon={
-                      <Ionicons
-                        name={"clipboard-outline"}
-                        size={24}
-                        color={"black"}
-                      />
-                    }
-                  />
-                    <IconButton
-                        onPress={() => {
-                            (navigation as any).navigate("slides", {
-                                recording: recordings[index],
-                            });
-                        }}
-                        icon={
-                            <Ionicons
-                                name={"albums-outline"}
-                                size={24}
-                                color={"black"}
-                            />
-                        }
-                    />
-                </HStack>
-              </HStack>
-            );
-          })}
-        </ScrollView>
-      </Center>
-      <Button
-        onPress={() => {
-          signOut(auth)
-            .then()
-            .catch((error) => {
-              toast.show({
-                title: error.code,
-                description: error.message,
-                backgroundColor: "red.600",
-              });
-            });
-        }}
-      >
-        Logout
-      </Button>
-    </Box>
+                  }}>
+                        <Image source={logout} alt="Squirrel Logo" width={"100%"} height={"40px"} />
+                  </TouchableOpacity>
+              </Box>
+          </View>
+      </ImageBackground>
+
   );
 }
