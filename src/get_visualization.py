4c6f9d0f820bde1220fd5757638033bb134b375b
Sarah <sarxhlwalker@gmail.com>
2022-09-18 01:47:51 -0400

    Update get_visualization.py

--- src/get_visualization.py
+++ src/get_visualization.py
@@ -97,11 +97,11 @@ def initialize_nodes(flashcards: List[Flashcard], flashcard_dict: Dict[str, Flas
         if flashcard.first is not None:
             first = flashcard_dict[flashcard.first]  # type(first) == Flashcard
             first_node = temp_nodes[first]  # type(first_node) = Node
-            temp_nodes[first].point_in.add(first_node)  # Node points to Node
-        if flashcard.first is not None:
+            temp_nodes[first].point_in.add(first_node)  # first touched by flashcard
+        if flashcard.second is not None:
             second = flashcard_dict[flashcard.second]
             second_node = temp_nodes[second]
-            temp_nodes[second].point_in.add(second_node)
+            temp_nodes[second].point_in.add(second_node)  # second touched by flashcard
 
     nodes = {temp_nodes[flashcard] for flashcard in temp_nodes}
     return nodes
