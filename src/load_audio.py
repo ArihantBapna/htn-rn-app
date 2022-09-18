1c6f6cc8c9e186f1614f874d728db463624b419a
stereosteam <moabid75@gmail.com>
2022-09-18 03:21:26 -0400

    api key swap

--- src/load_audio.py
+++ src/load_audio.py
@@ -13,7 +13,7 @@ def read_file(filename, chunk_size=5242880):
 
 
 def get_response_from_url(url):
-    headers = {'authorization': "b479f0aa918d4566aaacdec3f82c9b54", 'content-type': "application/json"}
+    headers = {'authorization': "c4187da13e3d4c6fa7ead1cac4246a1f", 'content-type': "application/json"}
     endpoint = "https://api.assemblyai.com/v2/transcript"
     json = {
         "audio_url": url,
@@ -35,42 +35,3 @@ def get_response_from_url(url):
     if curr_status == 'error':
         return {"error": "error"}
     return response.json()
-
-
-def get_response(file):
-    headers = {'authorization': "b479f0aa918d4566aaacdec3f82c9b54"}
-    response = requests.post('https://api.assemblyai.com/v2/upload',
-                            headers=headers,
-                            data=read_file(file))
-    res = response.json()  # upload audio
-    if res['upload_url']:
-        endpoint = "https://api.assemblyai.com/v2/transcript"
-        headers['content-type'] = "application/json"
-        print(headers)
-        json = {
-            "audio_url": res['upload_url'],
-            "disfluencies": True,  # TODO: test on audio file that contains filler
-            "speaker_labels": True,  # TODO: test on audio file that (a) has multiple speakers one after the other;
-                                    # TODO: (b) overlapping speakers
-            "auto_chapters": True,   # TODO: test
-            "entity_detection": True,  # TODO: test
-        }
-        response = requests.post(endpoint, json=json, headers=headers)
-        print(response.json())  # begin transcript process
-
-        transcript_id = response.json()['id']  # get reqs until success
-        curr_status = response.json()['status']
-        endpoint = f"https://api.assemblyai.com/v2/transcript/{transcript_id}"
-        headers = {
-            "authorization": "b479f0aa918d4566aaacdec3f82c9b54",
-        }
-        while curr_status != 'completed' and curr_status != 'error':
-            response = requests.get(endpoint, headers=headers)
-            curr_status = response.json()['status']
-            print(curr_status)
-
-        if curr_status == 'error':
-            print("you suck")
-            return
-        return response.json()
-    return "you also suck"
